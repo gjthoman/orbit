afb66495c4bcc822835ff61ec4308386e0040dab
Greg Thoman <greg.thoman@daveramsey.com>
2016-06-12 16:43:23 -0500

    Added Date with visibility and color options;

--- src/app.js
+++ src/app.js
@@ -1,12 +1,94 @@
 Pebble.addEventListener('showConfiguration', function() {
   var back = window.localStorage.getItem('BACKGROUND_COLOR') ? window.localStorage.getItem('BACKGROUND_COLOR') : 0;
-  var hour1 = window.localStorage.getItem('HOUR_1_COLOR') ? window.localStorage.getItem('HOUR_1_COLOR') : 5592405;
-  var hour2 = window.localStorage.getItem('HOUR_2_COLOR') ? window.localStorage.getItem('HOUR_2_COLOR') : 5592405;
-  var min1 = window.localStorage.getItem('MIN_1_COLOR') ? window.localStorage.getItem('MIN_1_COLOR') : 5592405;
-  var min2 = window.localStorage.getItem('MIN_2_COLOR') ? window.localStorage.getItem('MIN_2_COLOR') : 5592405;
+  var hour1 = window.localStorage.getItem('HOUR_1_COLOR') ? window.localStorage.getItem('HOUR_1_COLOR') : 65280;
+  var hour2 = window.localStorage.getItem('HOUR_2_COLOR') ? window.localStorage.getItem('HOUR_2_COLOR') : 65280;
+  var min1 = window.localStorage.getItem('MIN_1_COLOR') ? window.localStorage.getItem('MIN_1_COLOR') : 65280;
+  var min2 = window.localStorage.getItem('MIN_2_COLOR') ? window.localStorage.getItem('MIN_2_COLOR') : 65280;
+  var date = window.localStorage.getItem('DATE_COLOR') ? window.localStorage.getItem('DATE_COLOR') : 11184810;  
+  var batt_b = window.localStorage.getItem('BATT_BACK_COLOR') ? window.localStorage.getItem('BATT_BACK_COLOR') : 5592405;
+  var batt_f = window.localStorage.getItem('BATT_FORE_COLOR') ? window.localStorage.getItem('BATT_FORE_COLOR') : 11184810;
+  var show_batt = window.localStorage.getItem('SHOW_BATT') ? window.localStorage.getItem('SHOW_BATT') : 1;
+  var show_date = window.localStorage.getItem('SHOW_DATE') ? window.localStorage.getItem('SHOW_DATE') : 2;
   
-  var url = 'http://gjthoman.github.io/orbit/config.html?background_color=' + back + '&hour_1_color=' + hour1 + '&hour_2_color=' + hour2 + '&min_1_color=' + min1 + '&min_2_color=' + min2 
+ var config = [  
+  {
+    type: "title",
+    label: "Orbit Watchface"
+  },
+  {
+    type: "option",
+    key: "batt_visibility",
+    label: "Battery Visibility",
+    default: show_batt,
+    options: [ "Always Show", "Never Show", "Show on Shake"]
+  },
+  {
+    type: "option",
+    key: "date_visibility",
+    label: "Date Visibility",
+    default: show_date,
+    options: [ "Always Show", "Never Show", "Show on Shake"]
+  },
+  {
+    type: "section",
+    label: "Time Colors"
+  },
+  {
+    type: "color",
+    key: "background_color",
+    label: "Background",
+    default: back
+  },
+  {
+    type: "color",
+    key: "hour_1_color",
+    label: "Hours: First Digit",
+    default: hour1
+  },
+  {
+    type: "color",
+    key: "hour_2_color",
+    label: "Hours: Second Digit",
+    default: hour2
+  },
+  {
+    type: "color",
+    key: "min_1_color",
+    label: "Minutes: First Digit",
+    default: min1
+  },
+  {
+    type: "color",
+    key: "min_2_color",
+    label: "Minutes: Second Digit",
+    default: min2
+  },
+  {
+    type: "section",
+    label: "Indicator Colors"
+  },
+   {
+    type: "color",
+    key: "date_color",
+    label: "Date Text",
+    default: date
+  },
+  {
+    type: "color",
+    key: "battery_foreground_color",
+    label: "Battery Foreground",
+    default: batt_f
+  },
+  {
+    type: "color",
+    key: "battery_background_color",
+    label: "Battery Background",
+    default: batt_b
+  }
+];
 
+  var url = 'http://gjthoman.github.io/pebble_auto_config/1.0.2?config=' + encodeURIComponent(JSON.stringify(config));  
+  
   Pebble.openURL(url);
 });
 
@@ -20,14 +102,24 @@ Pebble.addEventListener('webviewclosed', function(e) {
     'AppKeyHour1Color': configData.hour_1_color,
     'AppKeyHour2Color': configData.hour_2_color,
     'AppKeyMin1Color': configData.min_1_color,
-    'AppKeyMin2Color': configData.min_2_color
+    'AppKeyMin2Color': configData.min_2_color,
+    'AppKeyBattBackColor': configData.battery_background_color,
+    'AppKeyBattForeColor': configData.battery_foreground_color,
+    'AppKeyShowBatt': parseInt(configData.batt_visibility),
+    'AppKeyShowDate': parseInt(configData.date_visibility),
+    'AppKeyDateColor': configData.date_color
   };
-  
+
   window.localStorage.setItem('BACKGROUND_COLOR', configData.background_color);
   window.localStorage.setItem('HOUR_1_COLOR', configData.hour_1_color);
-  window.localStorage.setItem('HOUR_1_COLOR', configData.hour_1_color);
+  window.localStorage.setItem('HOUR_2_COLOR', configData.hour_2_color);
   window.localStorage.setItem('MIN_1_COLOR', configData.min_1_color);
   window.localStorage.setItem('MIN_2_COLOR', configData.min_2_color);
+  window.localStorage.setItem('DATE_COLOR', configData.date_color);
+  window.localStorage.setItem('BATT_BACK_COLOR', configData.battery_background_color);
+  window.localStorage.setItem('BATT_FORE_COLOR', configData.battery_foreground_color);
+  window.localStorage.setItem('SHOW_BATT', parseInt(configData.batt_visibility));
+  window.localStorage.setItem('SHOW_DATE', parseInt(configData.date_visibility));
   
   // Send to the watchapp
   Pebble.sendAppMessage(dict, function() {
--- src/simple_analog.c
+++ src/simple_analog.c
@@ -3,13 +3,46 @@
 #include "pebble.h"
 
 static Window *s_window;
-static Layer *s_simple_bg_layer, *s_hands_layer;
+static Layer *s_simple_bg_layer, *s_hands_layer, *s_date_layer;
+static TextLayer *s_day_label, *s_num_label;
 static int s_digits[12];
+static char s_num_buffer[4], s_day_buffer[6];
+
+static GColor background_color;
+static GColor hour_1_color;
+static GColor hour_2_color;
+static GColor min_1_color;
+static GColor min_2_color;
+static GColor date_color;
+static GColor batt_back_color;
+static GColor batt_fore_color;
+
+static int battery_visibility;
+static int date_visibility;
+/*
+0: Always
+1: Never
+2: Shake
+*/
+
+static bool show_battery;
+
+typedef enum {
+  AppKeyBackgroundColor = 0,
+  AppKeyHour1Color = 1,
+  AppKeyHour2Color = 2,
+  AppKeyMin1Color = 3,
+  AppKeyMin2Color = 4,
+  AppKeyBattBackColor = 5,
+  AppKeyBattForeColor = 6,
+  AppKeyShowBatt = 7,
+  AppKeyShowDate = 8,
+  AppKeyDateColor = 9
+} AppKey;
 
 static void bg_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, background_color);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
-  graphics_context_set_stroke_color(ctx, GColorWhite);
 }
 
 static GPoint getPoint(int32_t angle) {
@@ -21,11 +54,45 @@ static GPoint getPoint(int32_t angle) {
   return GPoint((int16_t)(sin_lookup(angle) * (int32_t)radius / TRIG_MAX_RATIO) + center.x, (int16_t)(-cos_lookup(angle) * (int32_t)radius / TRIG_MAX_RATIO) + center.y);
 }
 
+static void drawBatt(Layer *layer, GContext *ctx) {
+  
+    GRect bounds = layer_get_bounds(layer);
+    BatteryChargeState battState = battery_state_service_peek();
+    
+    int batt_perc = battState.charge_percent;
+    
+    float height = bounds.size.h * batt_perc/100;
+    float width = bounds.size.w * batt_perc/100;
+    
+    float h_diff = (bounds.size.h - height)/2;
+    float w_diff = (bounds.size.w - width)/2;
+  
+    int offset = (bounds.size.h - bounds.size.w)/2;
+    
+    GPoint y_a = GPoint(bounds.size.w/2, h_diff+offset);
+    GPoint y_b = GPoint(bounds.size.w/2, bounds.size.h - h_diff-offset);
+    GPoint x_a = GPoint(w_diff, bounds.size.h/2);
+    GPoint x_b = GPoint(bounds.size.w - w_diff, bounds.size.h/2);
+    
+    graphics_context_set_stroke_width(ctx, 4);
+    graphics_context_set_stroke_color(ctx, batt_back_color);
+    graphics_context_set_fill_color(ctx, batt_back_color);
+    
+    graphics_draw_line(ctx, GPoint(bounds.size.w/2, 0+offset), GPoint(bounds.size.w/2, bounds.size.h-offset));
+    graphics_draw_line(ctx, GPoint(0, bounds.size.h/2), GPoint(bounds.size.w, bounds.size.h/2));
+    
+    graphics_context_set_stroke_width(ctx, 4);
+    graphics_context_set_stroke_color(ctx, batt_fore_color);
+    graphics_context_set_fill_color(ctx, batt_fore_color);
+    
+    graphics_draw_line(ctx, y_a, y_b);
+    graphics_draw_line(ctx, x_a, x_b);
+  
+}
+
 static void draw_segment(Layer *layer, GContext *ctx,  int position, int segment) {
   GRect bounds = layer_get_bounds(layer);
   graphics_context_set_stroke_width(ctx, 5);
-  graphics_context_set_stroke_color(ctx, GColorGreen);
-  graphics_context_set_fill_color(ctx, GColorGreen);
 
   uint16_t inset_thickness = PBL_IF_ROUND_ELSE(7, 6); 
   
@@ -203,9 +270,39 @@ static void draw_segment(Layer *layer, GContext *ctx,  int position, int segment
   }
 }
 
+char *upcase(char *str) {
+  for (int i = 0; str[i] != 0; i++) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      str[i] -= 0x20;
+    }
+  }
+  
+  return str;
+}
+
 static void hands_update_proc(Layer *layer, GContext *ctx) {  
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
+
+  if( date_visibility != 1) {
+    if (date_visibility == 0 || show_battery) {
+      text_layer_set_text_color(s_day_label, date_color);
+      text_layer_set_text_color(s_num_label, date_color);
+      
+      strftime(s_day_buffer, sizeof(s_day_buffer), "%b", t);
+      text_layer_set_text(s_day_label, upcase(s_day_buffer));
+    
+      strftime(s_num_buffer, sizeof(s_num_buffer), "%d", t);
+      text_layer_set_text(s_num_label, s_num_buffer);
+    
+    } else {
+      text_layer_set_text_color(s_day_label, GColorClear);
+      text_layer_set_text_color(s_num_label, GColorClear);
+    }
+  } else {
+    text_layer_set_text_color(s_day_label, GColorClear);
+    text_layer_set_text_color(s_num_label, GColorClear);
+  }
 
   int mins = t->tm_min;
   int hours = t->tm_hour;
@@ -214,7 +311,7 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
     if (hours > 12){
       hours = hours - 12;
     }
-  }
+  } 
   
   if(mins < 10) {
     s_digits[2] = 0;
@@ -230,18 +327,96 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
   }
   s_digits[1] = hours % 10;
   
+  if (battery_visibility != 1) {
+    if (battery_visibility == 0 || show_battery) {
+      drawBatt(layer, ctx);
+    }  
+  }
+  
+  graphics_context_set_stroke_color(ctx, hour_1_color);
+  graphics_context_set_fill_color(ctx, hour_1_color);
   for (int i = 0; i < NUM_POINTS[s_digits[0]]; i ++ ) {
     draw_segment(layer, ctx, 1, NUM_PATHS[s_digits[0]][i]);
   }
+  
+  graphics_context_set_stroke_color(ctx, hour_2_color);
+  graphics_context_set_fill_color(ctx, hour_2_color);
   for (int i = 0; i < NUM_POINTS[s_digits[1]]; i ++ ) {
     draw_segment(layer, ctx, 2, NUM_PATHS[s_digits[1]][i]);
   }
+  
+  graphics_context_set_stroke_color(ctx, min_1_color);
+  graphics_context_set_fill_color(ctx, min_1_color);
   for (int i = 0; i < NUM_POINTS[s_digits[2]]; i ++ ) {
     draw_segment(layer, ctx, 3, NUM_PATHS[s_digits[2]][i]);
   }
+  
+  graphics_context_set_stroke_color(ctx, min_2_color);
+  graphics_context_set_fill_color(ctx, min_2_color);
   for (int i = 0; i < NUM_POINTS[s_digits[3]]; i ++ ) {
     draw_segment(layer, ctx, 4, NUM_PATHS[s_digits[3]][i]);
   }
+}
+
+static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+  
+  Tuple *bg_color_t = dict_find(iter, AppKeyBackgroundColor);
+  if(bg_color_t) {
+    background_color = GColorFromHEX(bg_color_t->value->int32);
+    persist_write_int(AppKeyBackgroundColor, bg_color_t->value->int32);
+  }
+  
+  Tuple *hour_1_color_t = dict_find(iter, AppKeyHour1Color);
+  if(hour_1_color_t) {
+    hour_1_color = GColorFromHEX(hour_1_color_t->value->int32);
+    persist_write_int(AppKeyHour1Color, hour_1_color_t->value->int32);
+  }
+  Tuple *hour_2_color_t = dict_find(iter, AppKeyHour2Color);
+  if(hour_2_color_t) {
+    hour_2_color = GColorFromHEX(hour_2_color_t->value->int32);
+    persist_write_int(AppKeyHour2Color, hour_2_color_t->value->int32);
+  }
+  Tuple *min_1_color_t = dict_find(iter, AppKeyMin1Color);
+  if(min_1_color_t) {
+    min_1_color = GColorFromHEX(min_1_color_t->value->int32);
+    persist_write_int(AppKeyMin1Color, min_1_color_t->value->int32);
+  }
+  Tuple *min_2_color_t = dict_find(iter, AppKeyMin2Color);
+  if(min_2_color_t) {
+    min_2_color = GColorFromHEX(min_2_color_t->value->int32);
+    persist_write_int(AppKeyMin2Color, min_2_color_t->value->int32);
+  }
+
+  Tuple *date_color_t = dict_find(iter, AppKeyDateColor);
+  if(date_color_t) {
+    date_color = GColorFromHEX(date_color_t->value->int32);
+    persist_write_int(AppKeyDateColor, date_color_t->value->int32);
+  }
+  
+  Tuple *batt_fore_color_t = dict_find(iter, AppKeyBattForeColor);
+  if(batt_fore_color_t) {
+    batt_fore_color = GColorFromHEX(batt_fore_color_t->value->int32);
+    persist_write_int(AppKeyBattForeColor, batt_fore_color_t->value->int32);
+  }
+  
+  Tuple *batt_back_color_t = dict_find(iter, AppKeyBattBackColor);
+  if(batt_back_color_t) {
+    batt_back_color = GColorFromHEX(batt_back_color_t->value->int32);
+    persist_write_int(AppKeyBattBackColor, batt_back_color_t->value->int32);
+  }
+  Tuple *show_batt_t = dict_find(iter, AppKeyShowBatt);
+  if(show_batt_t) {
+    battery_visibility = show_batt_t->value->int32;
+    persist_write_int(AppKeyShowBatt, show_batt_t->value->int32);
+  }
+  
+  Tuple *show_date_t = dict_find(iter, AppKeyShowDate);
+  if(show_date_t) {
+    date_visibility = show_date_t->value->int32;
+    persist_write_int(AppKeyShowDate, show_date_t->value->int32);
+  }
+  
+  layer_mark_dirty(window_get_root_layer(s_window));
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
@@ -259,14 +434,94 @@ static void window_load(Window *window) {
   s_hands_layer = layer_create(bounds);
   layer_set_update_proc(s_hands_layer, hands_update_proc);
   layer_add_child(window_layer, s_hands_layer);
+  
+  s_date_layer = layer_create(bounds);
+
+  layer_add_child(window_layer, s_date_layer);
+
+  s_day_label = text_layer_create(PBL_IF_ROUND_ELSE(
+    GRect(112, 105, 27, 24),
+    GRect(85, 93, 27, 24)));
+
+  text_layer_set_text(s_day_label, s_day_buffer);
+  text_layer_set_background_color(s_day_label, GColorClear);
+  text_layer_set_text_color(s_day_label, GColorClear);
+  text_layer_set_font(s_day_label, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+
+  layer_add_child(s_date_layer, text_layer_get_layer(s_day_label));
+
+  s_num_label = text_layer_create(PBL_IF_ROUND_ELSE(
+    GRect(139, 105, 18, 24),
+    GRect(112, 93, 18, 24)));
+  text_layer_set_text(s_num_label, s_num_buffer);
+  text_layer_set_background_color(s_num_label, GColorClear);
+  text_layer_set_text_color(s_num_label, GColorClear);
+  text_layer_set_font(s_num_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+
+  layer_add_child(s_date_layer, text_layer_get_layer(s_num_label));
 }
 
 static void window_unload(Window *window) {
   layer_destroy(s_simple_bg_layer);
   layer_destroy(s_hands_layer);
 }
+static void reset_shake() {
+  show_battery = false;
+  layer_mark_dirty(window_get_root_layer(s_window));
+}
+
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+  if (battery_visibility == 2){
+    show_battery = true;
+    
+    layer_mark_dirty(window_get_root_layer(s_window));
+    app_timer_register(8000, reset_shake, NULL);  
+  }
+}
+
+static void handle_battery(BatteryChargeState charge_state) {
+  layer_mark_dirty(window_get_root_layer(s_window));
+}
+
+static GColor readColor(AppKey app_key, GColor defaultColor) {
+  if (persist_exists(app_key)) {
+    return GColorFromHEX(persist_read_int(app_key));    
+  } else {
+    return defaultColor;
+  }
+}
+
+static bool readBool(AppKey app_key, bool _bool) {
+  if (persist_exists(app_key)) {
+    return persist_read_bool(app_key);    
+  } else {
+    return _bool;
+  }
+}
+
+static int readInt(AppKey app_key, int _int) {
+  if (persist_exists(app_key)) {
+    return persist_read_int(app_key);
+  } else {
+    return _int;
+  }
+}
 
 static void init() {
+  background_color = readColor(AppKeyBackgroundColor, GColorBlack);
+  hour_1_color = readColor(AppKeyHour1Color, GColorGreen);  
+  hour_2_color = readColor(AppKeyHour2Color, GColorGreen);  
+  min_1_color = readColor(AppKeyMin1Color, GColorGreen);  
+  min_2_color = readColor(AppKeyMin2Color, GColorGreen);  
+  date_color = readColor(AppKeyDateColor, GColorWhite);  
+  batt_back_color = readColor(AppKeyBattBackColor, GColorDarkGray);
+  batt_fore_color = readColor(AppKeyBattForeColor, GColorLightGray);
+  
+  battery_visibility = readInt(AppKeyShowBatt, 2);
+  date_visibility = readInt(AppKeyShowDate, 2);
+  
+  show_battery = false;
+  
   s_window = window_create();
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = window_load,
@@ -275,6 +530,12 @@ static void init() {
   window_stack_push(s_window, true);
 
   tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  accel_tap_service_subscribe(tap_handler);
+  battery_state_service_subscribe(handle_battery);
+  
+  app_message_register_inbox_received(inbox_received_handler);
+
+  app_message_open(128, 128);
 }
 
 static void deinit() {
